6c6a4816445489a1d444559bae0e4ebc420087e9
Marwan Mohamed <marawanmohamed9876@gmail.com>
2024-07-18 15:27:09 +0300

    feat: finished the real sink a startup game

--- real sink a startup/src/Main.java
+++ real sink a startup/src/Main.java
@@ -2,6 +2,9 @@
 public class Main {
     public static void main(String[] args) {
 
-        }
+        StartupBust game = new StartupBust();
+        game.setUpGame();
+        game.startPlaying();
+
     }
 }
